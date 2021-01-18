267a90871ef9e3a23600c49b33325cd0d3e46593
ndv0 <ndv0@pop-os.localdomain>
2021-01-17 16:35:02 -0800

    Multer Upload Middleware

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// now use multer
+const uploadMulter = require('../middlewares/upload.js');
+
 // Controller
 const { createCategory } = require('../controllers/category.controllers.js');
 
-router.post('/category', createCategory);
+router.post('/category', uploadMulter, createCategory);
 
 module.exports = router 
